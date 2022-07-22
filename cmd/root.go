c3e428875e3b66a6b08e7196ce6aa6a1d3aae056
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-07-22 10:05:26 +0200

    Added root command and config.

---
+++ cmd/root.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const testkubeWatchConfigFile = ".testkube-watch.yaml"
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "testkube-watch-controller",
+	Short: "Run Testkube test on Kubernetes events",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Disable Help subcommand
+	RootCmd.SetHelpCommand(&cobra.Command{
+		Use:    "no-help",
+		Hidden: true,
+	})
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.testkube-watch.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(testkubeWatchConfigFile) // name of config file (without extension)
+	viper.AddConfigPath("$HOME")                 // adding home directory as first search path
+	viper.AutomaticEnv()                         // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lreimer/testkube-event-controller/cmd"
+
+func main() {
+	cmd.Execute()
+}
